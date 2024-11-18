2c698e265abd330406519f4017986b826f079521
cjkangme <nureongi0214@gmail.com>
2024-11-19 00:29:04 +0900

    feat: mouse drag object rotation control

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export * from './controls/KeyboardObjectRotationControls';
 export * from './controls/MouseDragCameraRotationControls';
+export * from './controls/MouseDragObjectRotationControls';
 
 export * from './helpers/PhysicsControlsHelper';
