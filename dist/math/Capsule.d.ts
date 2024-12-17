90034b59a41ffa6aa45ef9f504fecf99ee5d5a68
Byongho96 <Byongho96@users.noreply.github.com>
2024-12-17 10:13:33 +0000

    deploy: be6f1ac461c5ddd321769aeef0c9bef58f32521e

--- dist/math/Capsule.d.ts
+++
@@ -1,7 +0,0 @@
-import { Vector3 } from 'three';
-import { Capsule } from 'three/examples/jsm/math/Capsule.js';
-declare class ColliderCapsule extends Capsule {
-    constructor(start?: Vector3, end?: Vector3, radius?: number);
-    get height(): number;
-}
-export { ColliderCapsule };
